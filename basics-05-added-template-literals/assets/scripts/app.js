e1d741c078880070dbbd5e4c723a06ee2e0968d4
jangwonm <mjw0054@gmail.com>
2023-01-19 22:36:42 +0900

    update

--- basics-05-added-template-literals/assets/scripts/app.js
+++ basics-05-added-template-literals/assets/scripts/app.js
@@ -1,18 +1,14 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(num1, num2){
-    const result = num1 + num2;
-    //alert('The result is ' + result);
-    return result;
+function add(){
+    currentResult = currentResult + userInput.value;
+    outputResult(currentResult, '');
+    /*const result = currentResult + userInput.value;
+    alert('The result is ' + result);
+    return result;*/
 }
 
-//sconst additionResult =  add(1, 2);
-currentResult =  add(1, 2);
+addBtn.addEventListener('click', add);
 
-//currentResult = (currentResult + 10) * 3 / 2 - 1;
-//currentResult = additionResult
 
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
-
-outputResult(currentResult, calculationDescription);
