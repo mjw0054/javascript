1dc7f89e7a3d8d5e8b9ccf4f5acd3b8925bed211
jangwonm <mjw0054@gmail.com>
2023-01-19 23:43:05 +0900

    updagte

--- basics-05-added-template-literals/assets/scripts/app.js
+++ basics-05-added-template-literals/assets/scripts/app.js
@@ -1,42 +1,43 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+// 입력 필드에서 입력 값을 가져옴
 function getUserNumberInput() {
-    return parseInt(userInput.value)
+    return parseInt(userInput.value) //parseInt(userInput.value) == +userInput.value
 }
 
+// 계산 로그 생성과 작성
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
     const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
-    //const calcDescription = currentResult + resultBeforeCalc + calcNumber;
-    outputResult(currentResult, calcDescription);
+    // const calcDescription = currentResult + resultBeforeCalc + calcNumber;
+    outputResult(currentResult, calcDescription); // outputResult 함수는 vendor.js에서 호출
 }
 
 function add(){
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult + enteredNumber; 
-    //parseInt(userInput.value) == +userInput.value
+    currentResult += enteredNumber; // currentResult = currentResult + enteredNumber; 
     createAndWriteOutput('+', initialResult, enteredNumber)
 }
 
 function subtract() {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult - enteredNumber;
+    currentResult -= enteredNumber;
     createAndWriteOutput('-', initialResult, enteredNumber)
 }
 
 function multiply() {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult * enteredNumber;
+    currentResult *= enteredNumber;
     createAndWriteOutput('*', initialResult, enteredNumber)
 }
 
 function divide() {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult / enteredNumber;
+    currentResult /= enteredNumber;
     createAndWriteOutput('/', initialResult, enteredNumber)
 }
 
